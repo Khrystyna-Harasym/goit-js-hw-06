21a4b93400bb98b02bbf2346184211733ba9a5e0
Khrystyna Harasym <109437395+Khrystyna-Harasym@users.noreply.github.com>
2022-12-04 12:23:27 -0500

    update task -09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const btn = document.querySelector('.change-color');
+const btn = document.querySelector(".change-color");
 const color = document.querySelector('.color');
+
+
 
 btn.addEventListener('click', changeBgdColor);
 
 function changeBgdColor() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  color.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
 }
+
